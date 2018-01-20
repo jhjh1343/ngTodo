acbee4ca43cc359499d7613190c6362b14df52c7
moon <mwj1st@naver.com>
2018-01-20 18:29:47 +0900

    20180120 마우스오버시에 색 변하게

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+  @Input() highlightColor: string;
 
-  constructor() { }
+  constructor(private el: ElementRef) {
+    // el.nativeElement까지가 javascript node이다
+    // el.nativeElement.style.backgroundColor = 'yellow';
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.highlightColor || 'red');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 
 }

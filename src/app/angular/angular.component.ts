af50c577adeffc6defc155bcdf5aced0c81d146d
moon <mwj1st@naver.com>
2018-01-27 10:16:48 +0900

    20180127 jquery angular 연동

--- src/app/angular/angular.component.ts
+++ src/app/angular/angular.component.ts
@@ -18,7 +18,7 @@ import {ResultVo} from '../domain/result.vo';
       transition('in => void', [
         animate(300, keyframes([
           style({opacity: 1, transform: 'translateX(0)', offset: 0}),
-          style({opacity: 1, transform: 'translateX(-50px)', offset: 0.7}),  // 0.21초
+          style({opacity: 1, transform: 'translateX(-50px)', offset: 0.7}),  // 0.3 * 0.7 = 0.21초
           style({opacity: 0, transform: 'translateX(100%)', offset: 1.0})    // 0.3초
         ]))
       ]),
